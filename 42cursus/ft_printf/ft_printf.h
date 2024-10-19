9dced271a51585b727fc5b5a3a557debcf27058c
Siyoung Pyeon <austin8842@naver.com>
2024-10-19 17:13:46 +0900

    Study va_arg

--- 42cursus/ft_printf/ft_printf.h
+++ 42cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 03:03:46 by sipyeon           #+#    #+#             */
-/*   Updated: 2024/10/17 15:52:51 by sipyeon          ###   ########.fr       */
+/*   Updated: 2024/10/19 16:43:04 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 
 # include <unistd.h>
+# include <stdarg.h>
 # include "./libft/libft.h"
 
 int	ft_printf(const char *s, ...);
