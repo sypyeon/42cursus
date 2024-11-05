44bda5ce3453e76ba0991b4ff73f30880c442d62
sypyeon <austin8842@naver.com>
2024-11-05 15:23:54 +0900

    Modify

--- 42cursus/get_next_line/get_next_line.h
+++ 42cursus/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 21:37:32 by sipyeon           #+#    #+#             */
-/*   Updated: 2024/11/03 22:47:59 by sipyeon          ###   ########.fr       */
+/*   Updated: 2024/11/03 23:18:09 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 # define GET_NEXT_LINE__H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE
+#  define BUFFER_SIZE 42
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_str_join(char *dest, char *src);
+char	*ft_strdup(const char *s);
+
 
 #endif
