0bcc6165cfe1a4cb0763ded5b2e5981b59b0cdbe
Siyoung Pyeon <austin8842@naver.com>
2024-11-26 21:52:35 +0900

    Initiate Push_swap

---
+++ 42cursus/push_swap/push_swap.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 19:02:45 by sipyeon           #+#    #+#             */
+/*   Updated: 2024/11/26 21:50:03 by sipyeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_list
+{
+	int				nb;
+	struct s_list	*next;
+	struct s_list	*prev;	
+}	t_list;
+
+typedef struct	s_stack
+{
+	int		size;
+	t_list	*head;
+	t_list	*tail;
+}	t_stack;
+
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#endif
