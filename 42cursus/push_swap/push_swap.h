14e3142bf711c59cc3115ed383a4f15aa1f34d17
Siyoung Pyeon <austin8842@naver.com>
2024-11-26 23:54:12 +0900

    Making sorting_function

--- 42cursus/push_swap/push_swap.h
+++ 42cursus/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 19:02:45 by sipyeon           #+#    #+#             */
-/*   Updated: 2024/11/26 21:50:03 by sipyeon          ###   ########.fr       */
+/*   Updated: 2024/11/26 23:45:59 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct s_list
+typedef struct	s_list
 {
 	int				nb;
 	struct s_list	*next;
-	struct s_list	*prev;	
+	struct s_list	*prev;
 }	t_list;
 
 typedef struct	s_stack
--- 42cursus/push_swap/sorting_fuction.c
+++ 42cursus/push_swap/sorting_fuction.c
@@ -6,9 +6,42 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 21:41:08 by sipyeon           #+#    #+#             */
-/*   Updated: 2024/11/26 21:41:25 by sipyeon          ###   ########.fr       */
+/*   Updated: 2024/11/26 23:52:28 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_swap(t_stack *stack, int flag)
+{
+	t_list *head;
+	t_list *second;
+
+	if (stack->size < 2)
+		return ;
+	head = stack->head;
+	second = head->next;
+	head->next = second->next;
+	head->prev = second;
+	second->next = head;
+	second->prev = NULL;
+	if (flag == 1)
+		ft_printf("sa");
+	else if (flag == 2)
+		ft_printf("sb");
+}
+
+void	ft_push()
+{
+	
+}
+
+void	ft_rotate()
+{
+	
+}
+
+void	ft_rr()
+{
+	
+}
