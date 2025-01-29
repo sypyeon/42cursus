d32ca60f206d2db698f939379421f272be5b40b3
Siyoung Pyeon <austin8842@naver.com>
2025-01-29 19:33:51 +0900

    Starting pipex

---
+++ 42cursus/pipex/pipex.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 17:34:15 by sipyeon           #+#    #+#             */
+/*   Updated: 2025/01/29 17:58:11 by sipyeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PIPEX_H
+# define PIPEX_H
+
+#include "./libft/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef struct	s_cmd
+{
+	char **cmd;
+	struct s_cmd *next;
+	struct s_cmd *prev;
+}				t_cmd;
+
+typedef	struct	s_cmd_info
+{
+	int		size;
+	t_cmd	*head;
+	t_cmd	*tail;
+}				t_cmd_info;
+
+
+#endif
