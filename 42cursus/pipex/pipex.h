676febbb869f2b7518b3048c761860a9aa32d481
Siyoung Pyeon <austin8842@naver.com>
2025-01-30 23:12:49 +0900

    Starting to understand what to do.

--- 42cursus/pipex/pipex.h
+++ 42cursus/pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:34:15 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/01/30 16:26:59 by sipyeon          ###   ########.fr       */
+/*   Updated: 2025/01/30 23:00:11 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 typedef struct	s_cmd
 {
 	char 			**cmd;
+	int				pipe_fd[2];
 	struct s_cmd	*next;
 	struct s_cmd	*prev;
 }				t_cmd;
@@ -30,8 +31,9 @@ typedef struct	s_cmd
 typedef	struct	s_cmd_info
 {
 	int		size;
-	char	*in;
-	char	*out;
+	int		in_fd;
+	int		out_fd;
+	char	**path;
 	t_cmd	*head;
 	t_cmd	*tail;
 }				t_cmd_info;
