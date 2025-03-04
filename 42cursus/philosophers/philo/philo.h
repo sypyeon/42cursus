edb83bcc7d10ae702e3f1e3c280844bc3566cd28
Siyoung Pyeon <austin8842@naver.com>
2025-03-04 19:09:00 +0900

    Making function to lock n unlock mutex

--- 42cursus/philosophers/philo/philo.h
+++ 42cursus/philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 22:01:28 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/03/03 17:42:05 by sipyeon          ###   ########.fr       */
+/*   Updated: 2025/03/04 19:00:13 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@
 
 typedef struct s_mutex
 {
-	long			data;
+	long			status;
 	pthread_mutex_t	key;
 }	t_mutex;
 
@@ -51,11 +51,18 @@ typedef struct s_philo
 	int				no;
 	int				left_fork;
 	int				right_fork;
+	int				ready_to_eat;
 	t_mutex			eaten;
 	t_mutex			dead;
 }	t_philo;
 
 int	p_atoi(char *str);
 int	terminate_philo(int flag, t_philo **philo);
+
+// timestamp_in_ms X has taken a fork
+// timestamp_in_ms X is eating
+// timestamp_in_ms X is sleeping
+// timestamp_in_ms X is thinking
+// timestamp_in_ms X died
 
 #endif
--- 42cursus/philosophers/philo/philo_main.c
+++ 42cursus/philosophers/philo/philo_main.c
@@ -6,41 +6,103 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 19:39:54 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/03/04 00:15:22 by sipyeon          ###   ########.fr       */
+/*   Updated: 2025/03/04 18:59:17 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	ph_reach_fork(t_philo *philo, t_philo_data *data)
+{
+	t_mutex	left_fork;
+	t_mutex	right_fork;
+	
+	left_fork = data->fork[philo->left_fork];
+	right_fork = data->fork[philo->right_fork];
+	while (1)
+	{
+		if (left_fork.status != 0 && right_fork.status != 0)
+		{
+			pthread_mutex_lock(&left_fork.key);
+			pthread_mutex_lock(&right_fork.key);
+			left_fork.status = philo->no;
+			right_fork.status = philo->no;
+			philo->ready_to_eat = 1;
+			break ;
+		}
+	}
+}
+void	ph_eat_and_release(t_philo *philo, t_philo_data *data)
+{
+	t_mutex	left_fork;
+	t_mutex	right_fork;
+	
+	left_fork = data->fork[philo->left_fork];
+	right_fork = data->fork[philo->right_fork];
+	if (philo->ready_to_eat)
+	{
+		usleep(philo->data->tt_eat);
+		left_fork.status = 0;
+		right_fork.status = 0;
+		philo->ready_to_eat = 0;
+		pthread_mutex_unlock(&left_fork.key);
+		pthread_mutex_unlock(&right_fork.key);
+	}
+}
+
 void	*philo_task(void *arg)
 {
 	t_philo *philo;
 	philo = (t_philo *)arg;
+
+	if ((philo->no % 2) == 0)
+		usleep(100);
+	while (1)
+	{
+		if(philo->data->eat_count <= philo->eaten.status)
+			break ;
+		ph_reach_fork(philo, philo->data);
+		ph_eat_and_release(philo, philo->data);
+	}
 }
 
 int	get_philo_data(t_philo_data *data, char **av)
 {
-	if (data->num_of_philo = p_atoi(av[1]) < 1); // = number_of_philosophers and number_of_thread
-	data->tt_die = p_atoi(av[2]); // = time_to_die (in milliseconds)
-	data->tt_eat = p_atoi(av[3]); // = time_to_eat (in milliseconds)
-	data->tt_sleep = p_atoi(av[4]); // = time_to_sleep (in milliseconds)
+	if (data->num_of_philo = p_atoi(av[1]) < 1);
+	data->tt_die = p_atoi(av[2]);
+	data->tt_eat = p_atoi(av[3]);
+	data->tt_sleep = p_atoi(av[4]);
 	if (av[5])
-		data->eat_count = p_atoi(av[5]); // = [number_of_times_each_philosopher_must_eat] (optional argument)
+		data->eat_count = p_atoi(av[5]);
 	else
 		data->eat_count = -1;
 }
 
-void	init_mutex(t_philo *philo, t_philo_data *data)
+void	init_mutex(t_philo **philo, t_philo_data *data)
 {
 	int	i;
 
+	i = 0;
 	while (i < data->num_of_philo)
 	{
-		pthread_mutex_init(&(data->fork->key), NULL);
-		pthread_mutex_init(&(philo->eaten), NULL);
-		pthread_mutex_init(&(philo->dead), NULL);
+		pthread_mutex_init(&(data->fork[i].key), NULL);
+		pthread_mutex_init(&(philo[i]->eaten.key), NULL);
+		pthread_mutex_init(&(philo[i]->dead.key), NULL);
 		i++;
 	}
+}
+
+int	create_philos(t_philo **philo, t_philo_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->num_of_philo)
+	{
+		pthread_create(philo[i]->tid, NULL, philo_task, philo[i]);
+		i++;
+	}
+	return (0);
 }
 
 int	init_philo(t_philo **philo, t_philo_data *data)
@@ -52,27 +114,21 @@ int	init_philo(t_philo **philo, t_philo_data *data)
 		return (-1);
 	memset(philo, 0, sizeof(t_mutex) * data->num_of_philo);
 	data->fork = (t_mutex *)malloc(sizeof(t_mutex) * data->num_of_philo);
+	if (!data->fork)
+		return (-1);
 	memset(data->fork, 0, sizeof(t_mutex) * data->num_of_philo);
 	while (i < data->num_of_philo)
 	{
 		(*philo)[i].data = data;
 		(*philo)[i].no = i + 1;
 		(*philo)[i].left_fork = i;
-		(*philo)[i].right_fork = i + 1;
-		(*philo)[i].eaten.data = 0;
-		(*philo)[i].dead.data = 0;
-		if (data->num_of_philo == i + 1)
-			(*philo)[i].right_fork = 0;
+		(*philo)[i].right_fork = (i + 1) % data->num_of_philo;
 		i++;
 	}
+	init_mutex(philo, data);
 	return (0);
 }
 
-int	create_thread()
-{
-	pthread_create(philo[i].tid, NULL, philo_task, philo[i]);
-}
-	
 int	main(int ac, char **av)
 {
 	t_philo			*philo;
@@ -84,6 +140,6 @@ int	main(int ac, char **av)
 		return (terminate_philo(1, philo));
 	if (init_philo(&philo, &data) == -1)
 		return (terminate_philo(1, philo));
-	if ()
+	if (create_philos(&philo, &data) == -1)
 		return (-1);
 }
