5d0bf1de9addf37033482698f606dbfbf65541dc
Siyoung Pyeon <austin8842@naver.com>
2025-04-21 17:07:15 +0900

    clearing data

--- 42cursus/so_long/so_long.h
+++ 42cursus/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 17:17:36 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/03/06 19:05:16 by sipyeon          ###   ########.fr       */
+/*   Updated: 2025/02/21 18:02:34 by sipyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,36 +45,36 @@
 
 typedef struct s_param
 {
-	int		x; //X좌표
-	int		y; //Y좌표
-	void	*player; //다시 보니 필요 없을 수도
-}	t_param;
+	int		x;
+	int		y;
+	void	*player;
+}				t_param;
 
 typedef struct s_img
 {
-	void	*ptr; //이미지 포인터
-	char	*addr; //이미지 경로(디렉토리 및 파일 이름)
-	int		bpp; //뭔지 모름 도큐멘트에 있었음
-	int		line_length; //모름
-	int		endian; //모름
-}	t_img;
+	void	*ptr;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}				t_img;
 
 typedef struct s_map
 {
-	char	**info; //맵을 char 2차원 배열로 저장
-	char	*flat; //맵을 일자로 저장 (길이 얼만지 확인)
-	int		x_len; //맵 가로길이
-	int		y_len; //맵 세로길이
-	int		size; //x * y
-	int		valid; //유효성 플래그
-	int		p_x; //플레이어 X좌표
-	int		p_y; //플레이어 Y좌표
-	int		player_count; //맵의 P 갯수
-	int		exit_count; //맵의 E 갯수
-	int		c_count; //맵의 C 갯수
-}	t_map;
+	char	**info;
+	char	*flat;
+	int		x_len;
+	int		y_len;
+	int		size;
+	int		valid;
+	int		p_x;
+	int		p_y;
+	int		player_count;
+	int		exit_count;
+	int		c_count;
+}				t_map;
 
-typedef struct s_vars //구조체 저장하고 있는 제일 큰 구조체 (모든 정보를 담고 있음)
+typedef struct s_vars
 {
 	void	*mlx;
 	void	*win;
@@ -83,21 +83,15 @@ typedef struct s_vars //구조체 저장하고 있는 제일 큰 구조체 (모�
 	t_param	location;
 	int		player_face;
 	int		move_count;
-}	t_vars;
-/**
- * int	x; //x좌표
- * int	y; //y좌표
- * int	valid_path; //출구 유효 플래그
- * int	valid_collect; //아이템 회수 유효성 플래그
- * int	**visited; //0으로 초기화한 2차원 배열 (방문할때마다 1로 초기화)
- */
-typedef struct s_check 
+}				t_vars;
+
+typedef struct s_check
 {
-	int	x; //x좌표
-	int	y; //y좌표
-	int	valid_path; //출구 유효 플래그
-	int	valid_collect; //아이템 회수 유효성 플래그
-	int	**visited; //0으로 초기화한 2차원 배열 (방문할때마다 1로 초기화)
+	int	x;
+	int	y;
+	int	valid_path;
+	int	valid_collect;
+	int	**visited;
 }	t_check;
 
 int		close_game(t_vars *game);
