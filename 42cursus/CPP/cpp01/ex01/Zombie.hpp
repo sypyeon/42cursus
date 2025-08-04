0a8141c6877c9106cb61aff6ffc6b27eb68fce1b
Siyoung Pyeon <austin8842@naver.com>
2025-08-04 11:50:17 +0900

    WIP: making zombies.

--- 42cursus/CPP/cpp01/ex01/Zombie.hpp
+++ 42cursus/CPP/cpp01/ex01/Zombie.hpp
@@ -12,7 +12,8 @@ public:
 	void setName(std::string name);
 };
 
-Zombie* newZombie(std::string name);
 void randomChump(std::string name);
+Zombie* newZombie(std::string name);
+Zombie* zombieHorde(int N, std::string name);
 
 #endif
