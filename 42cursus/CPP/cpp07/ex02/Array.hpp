c55b2f87959ebac89b14f1ac92194e2bf239c8ee
Siyoung Pyeon <austin8842@naver.com>
2026-02-02 20:40:50 +0900

    WIP: working on ex02. had to check subject pdf for the requirements

--- 42cursus/CPP/cpp07/ex02/Array.hpp
+++ 42cursus/CPP/cpp07/ex02/Array.hpp
@@ -1,6 +1,41 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+template <typename T>
+class Array
+{
+private:
+	T *array;
+	unsigned int n;
+public:
+	Array();
+	Array(unsigned int n);
+	Array(Array &other);
+	Array &operator=(Array &other);
+	~Array();
+};
 
+template <typename T>
+Array<T>::Array()
+{
+	this->array = new T[0];
+	this->n = 0;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	this->array = new T[n];
+	this->n = n;
+}
+
+template <typename T>
+Array<T>::Array(Array &other)
+{
+	this->array = new T[this->n];
+}
+
+template <typename T>
+Array<T>::~Array() {}
 
 #endif
