b816ab545c6792abc74dd4620852d95b116ab2ff
sypyeon <austin8842@naver.com>
2026-05-18 07:55:22 +0900

    WIP: cpp08 ex02 work in progress

---
+++ 42cursus/CPP/cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,21 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+#include <iterator>
+
+template <typename T>
+class MutantStack : std::stack<T>
+{
+private:
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
+};
+
+#endif
