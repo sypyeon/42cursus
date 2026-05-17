9d4d7694eb2ca0fdb03de4e7a50f84ade1a9da78
sypyeon <austin8842@naver.com>
2026-05-18 04:25:26 +0900

    Add: template easyfind in easyfind.hpp

--- 42cursus/CPP/cpp08/ex00/easyfind.hpp
+++ 42cursus/CPP/cpp08/ex00/easyfind.hpp
@@ -1,22 +1,20 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-class easyfind
-{
-private:
-	/* data */
-public:
-	easyfind(/* args */);
-	~easyfind();
-};
+#include <algorithm> // For std::find
+#include <stdexcept>  // For std::runtime_error
 
-easyfind::easyfind(/* args */)
-{
+template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+    // Search for the integer value in the container
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    
+    // If the value isn't found, std::find returns the end iterator
+    if (it == container.end()) {
+        throw std::runtime_error("Value not found in the container.");
+    }
+    
+    return it;
 }
-
-easyfind::~easyfind()
-{
-}
-
 
 #endif
