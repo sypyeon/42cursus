19b7e4160f4429dfaa7f80cafbf918f67d7b0365
sypyeon <austin8842@naver.com>
2026-05-18 03:44:27 +0900

    Init: inception and cpp08

---
+++ 42cursus/CPP/cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+class easyfind
+{
+private:
+	/* data */
+public:
+	easyfind(/* args */);
+	~easyfind();
+};
+
+easyfind::easyfind(/* args */)
+{
+}
+
+easyfind::~easyfind()
+{
+}
+
+
+#endif
