49805b0aefd379a11ef9887ab2f6100939d1284e
sypyeon <austin8842@naver.com>
2026-05-24 23:31:04 +0900

    improve test for ex02 in cpp08

--- 42cursus/CPP/cpp08/ex02/main.cpp
+++ 42cursus/CPP/cpp08/ex02/main.cpp
@@ -1,36 +1,61 @@
 #include "MutantStack.hpp"
 
 #include <stack>
+#include <list>
 #include <iostream>
 
 int main()
 {
-	std::stack<int> stack;
-	stack.push(123);
-	stack.top();
-	stack.size();
-	std::cout << stack.top() << std::endl;
-
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		std::cout << "==== testing MutantStack ====" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "current top of the stack: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "stack size: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		std::cout << "==testing iterator==" << std::endl;
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << std::endl;
 	}
-	std::stack<int> s(mstack);
+
+	{
+		std::cout << "==== testing MutantStack ====" << std::endl;
+		std::list<int> list;
+		list.push_back(5);
+		list.push_back(17);
+		std::cout << "current top of the stack: " << list.back() << std::endl;
+		list.pop_back();
+		std::cout << "stack size: " << list.size() << std::endl;
+		list.push_back(3);
+		list.push_back(5);
+		list.push_back(737);
+		//[...]
+		list.push_back(0);
+		std::cout << "==testing iterator==" << std::endl;
+		std::list<int>::iterator it = list.begin();
+		std::list<int>::iterator ite = list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return 0;
 }
